30478e9909994f63dbd3a72cfd1357865c5d4735
DeeFly <daniel.holm@majodang.se>
2024-10-25 14:51:11 +0200

    turns out gravload was correct the whole time

--- Kinematics hand-in/dynamics.m
+++ Kinematics hand-in/dynamics.m
@@ -4,23 +4,21 @@ L1 = Link('d', 0.352, 'a', 0.070, 'alpha', pi/2);
 L2 = Link('d', 0, 'a', 0.360, 'alpha', 0, 'offset', pi/2);
 L3 = Link('d', 0, 'a', 0.445, 'alpha', 0, 'offset', -pi/2);
 
+% Set the masses
 L1.m = 40;
 L2.m = 35;
 L3.m = 25;
+
+% Set center of gravity in the middle, has to be negative for some reason
+L2.r = [-L2.a/2 0 0];
+L3.r = [-L3.a/2 0 0];
 
 irb = SerialLink([L1 L2 L3], 'name', 'IRB140');
 
 q = deg2rad([0 10 40]);
 
-link2 = irb.A(2, q).t;
-link23 = irb.A([2 3], q).t;
-
-torque2 = L2.m/2 * link2(1)/1000 + L3.m/2 * link23(1)/1000;
-
-link3 = link23 - link2;
-
-torque3 = L3.m/2 * link3(1)/1000;
-
+% Specify gravity to be down, i.e. negative in z.
+irb.gravload(q, [0 0 -9.81])
 %% Trajectory
 
 q0 = deg2rad([10 20 30]);
